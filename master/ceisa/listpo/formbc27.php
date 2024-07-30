b1b0d5ce34b25155634eb8d21da7c642f92b6c59
RonyAgungAriBudiman <32225565+RonyAgungAriBudiman@users.noreply.github.com>
2024-07-30 17:51:58 +0700

    -

--- master/ceisa/listpo/formbc27.php
+++ master/ceisa/listpo/formbc27.php
@@ -375,7 +375,7 @@ $_POST["kodestatuspemilik"] = $data_pt[0]['KodeStatus']
 							<div class="card-header" style="border-bottom:solid 0.5px #31708f;">
 								<h4>Kemasan</h4>
 								<div class="col-sm-6 mb-2" style="float: right;">
-                                        <a href="javascript:void(0);" onclick="popup('nometer', 'master/ceisa/listso/add_kemasan.php', '1100', '500')">
+                                        <a href="javascript:void(0);" onclick="popup('nometer', 'master/ceisa/listpo/add_kemasan.php', '1100', '500')">
                                             <button class="btn btn-primary" type="button" style="border-radius: 0;"><i class="fa fa-plus"></i>Kemasan</button>
                                         </a>
                                 </div>
