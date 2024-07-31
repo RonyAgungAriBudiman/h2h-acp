63056ae33f90a428ab7c850c9dfaac84196b6126
RonyAgungAriBudiman <32225565+RonyAgungAriBudiman@users.noreply.github.com>
2024-07-31 09:50:50 +0700

    -

---
+++ master/ceisa/listpo/simpan27.php
@@ -0,0 +1,19 @@
+<?php
+$_POST["seri"] = "1";
+$sql_urut = "SELECT MAX(Urut) as Urut FROM BC_HEADER 
+	                      WHERE KodeDokumen = '27' AND  YEAR(TanggalPernyataan) = '" . date("Y", strtotime($_POST['tanggalpernyataan'])) . "' ";
+$data_urut = $sqlLib->select($sql_urut);
+$urut = $data_urut[0]['Urut'] + 1;
+$nomor = str_pad($urut, 6, '0', STR_PAD_LEFT);
+$_POST["nomor"] = $nomor;
+$_POST["urut"] = $urut;
+$_POST["nomoraju"] =  '0000' . $_POST['kodedokumenbc'] . '-' . substr($_POST['nomoridentitaspengusaha'], 0, 6) . '-' . date("Ymd", strtotime($_POST['tanggalpernyataan'])) . '-' . $_POST['nomor'];
+
+$nomoraju = $_POST["nomoraju"];
+$jmlrow = $_POST["jmlrow"];
+$jmldok = $_POST["jmldok"];
+$jmltarif = $_POST["jmltarif"];
+$jmlimpor = $_POST["jmlimpor"];
+$jmljam = $_POST['jmljam'];
+$jmlkemasan = $_POST['jmlkemasan'];
+$jmlkontainer = $_POST['jmlkontainer'];
